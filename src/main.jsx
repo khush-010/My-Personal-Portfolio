391d9c54387466d7983383f790314a87b33492af
Khush Dhameliya <khushdhameliya007@gmail.com>
2025-01-26 13:23:21 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { Analytics } from "@vercel/analytics/react"
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <>
+    <Analytics/>
     <App />
-  </React.StrictMode>
+  </>
 );
