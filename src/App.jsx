68fdb66a1bf1019631be94146fa582290d873297
Khush Dhameliya <khushdhameliya007@gmail.com>
2025-01-29 11:13:19 +0530

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
-import  ContactForm  from "./components/Contact/ContactForm";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
